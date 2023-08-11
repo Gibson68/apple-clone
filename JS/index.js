45aed42fa72399db2f0f335151112b2e1f830461
Gibson68 <112637235+Gibson68@users.noreply.github.com>
2023-08-11 05:54:13 -0700

    first commit

---
+++ JS/index.js
@@ -0,0 +1,37 @@
+const headContainer = document.querySelector('.head-container');
+var jsRef = document.querySelector('.js-reference');
+var menuItem = document.querySelector('.menu-item');
+var menu = document.querySelector('.menu');
+var headAnchor = document.getElementsByTagName('a');
+// var headContainer = document.querySelector('.head-container');
+// var headContainer = document.querySelector('.head-container');
+
+headContainer.style.width = '100%';
+
+
+// headContainer.onmouseover = function () {
+//     headContainer.style.height = '70vh';
+//     menu.style.display = 'flex'
+//     headContainer.style.transition = '0.8s';
+    
+    
+// //    var h3 = document.createElement('h3');
+// //     var isText = document.createTextNode('Home');
+// //     var h3Text = h3.appendChild(isText);
+
+// //    var h3Text = headContainer.appendChild(h3Text);
+// //    var firstText = jsRef.appendChild(h3Text);
+
+// //    firstText.style.margin = '50px'
+
+   
+    
+// }
+
+// headContainer.onmouseout = function () {
+//     headContainer.style.height = '60px';
+//     menu.style.display = 'flex';
+
+   
+  
+// }
